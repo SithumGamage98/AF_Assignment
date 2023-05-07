6e91c327b0b6dee01ee2e8b308ae6922a2bbb6f8
Lushani Odara Amarakoon <86650379+OdaraAmarakoon@users.noreply.github.com>
2023-05-07 23:34:27 +0530

    Update Hotel_model.js

--- api/models/Hotel_model.js
+++ api/models/Hotel_model.js
@@ -1,6 +1,7 @@
 import mongoose from 'mongoose';
 const { Schema } = mongoose;
 
+
 const HotelSchema = new mongoose.Schema({
   name: { type: String, required: true },
   type: { type: String, required: true },
