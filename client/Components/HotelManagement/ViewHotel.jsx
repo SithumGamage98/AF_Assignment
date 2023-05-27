4bfee4d664caac4b665f3e0e0e87f2d716574c1f
Sankalpa69 <100986253+Sankalpa69@users.noreply.github.com>
2023-05-27 09:17:02 +0530

    Added frontend updated files

---
+++ client/Components/HotelManagement/ViewHotel.jsx
@@ -0,0 +1,151 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { useParams } from 'react-router-dom';
+import Rating from '../Rating';
+import Button from 'react-bootstrap/esm/Button';
+import Footer_3 from '../layout/Footer_hotels';
+
+const ViewHotel = () => {
+  const { id } = useParams();
+  const [hotel, setBlog] = useState(null);
+
+  useEffect(() => {
+    const fetchBlog = async () => {
+      try {
+        const response = await axios.get(
+          `http://localhost:8060/hotels/hotel/view/${id}`
+        );
+        setBlog(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchBlog();
+  }, [id]);
+
+  if (!hotel) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div style={{ textAlign: 'center', height: '500px', marginTop: '10px' }}>
+      <br></br>
+      <br></br>
+      <div
+        className="info-container card"
+        style={{
+          float: 'left',
+          width: '40%',
+          margin: '10px',
+          maxHeight: '1000px',
+        }}
+      >
+        {Array.isArray(hotel.photos) &&
+          hotel.photos.map((photo, index) => (
+            <img
+              key={index}
+              src={
+                typeof photo !== 'undefined'
+                  ? require(`../HotelManagement/HotelImages/${photo}`)
+                  : 'Error'
+              }
+              style={{ margin: '10px', padding: '10px', objectFit: 'cover' }}
+              alt={`Photo ${index}`}
+            />
+          ))}
+      </div>
+      <div style={{ float: 'left', width: '50%', margin: '10px' }}>
+        <div
+          className="info-container card"
+          style={{ float: 'left', width: '60%', margin: '10px' }}
+        >
+          <h1 style={{ fontSize: '44px', margin: '0', marginBottom: '10px' }}>
+            <b>{hotel.name}</b>
+          </h1>
+          <hr></hr>
+          <Rating value={hotel.rating} readOnly />
+          <hr></hr>
+          <p style={{ fontSize: '18px', margin: '0', marginBottom: '10px' }}>
+            Type: <b>{hotel.type}</b>
+          </p>
+          <hr></hr>
+          <p
+            style={{
+              fontSize: '16px',
+              fontWeight: 'bold',
+              margin: '0',
+              marginBottom: '5px',
+            }}
+          >
+            Description:
+          </p>
+
+          <p style={{ fontSize: '16px', margin: '0', marginBottom: '10px' }}>
+            {hotel.description}
+          </p>
+          <hr></hr>
+          <p
+            style={{
+              fontSize: '16px',
+              fontWeight: 'bold',
+              margin: '0',
+              marginBottom: '5px',
+            }}
+          >
+            Address:
+          </p>
+
+          <p style={{ fontSize: '16px', margin: '0', marginBottom: '10px' }}>
+            {hotel.address}
+          </p>
+        </div>
+        <div
+          className="info-container card"
+          style={{ float: 'left', width: '30%', margin: '10px' }}
+        >
+          <p style={{ fontSize: '16px', margin: '0', marginBottom: '10px' }}>
+            Price: {hotel.cheapestprice}.00 LKR
+          </p>
+          <p style={{ fontSize: '16px', margin: '0', marginBottom: '10px' }}>
+            Status:{' '}
+            <p
+              style={{
+                backgroundColor: 'green',
+                width: '100px',
+                color: 'white',
+                borderRadius: '10px',
+                margin: '0 auto',
+              }}
+            >
+              {hotel.featured ? 'Available' : 'Unavailable'}
+            </p>
+          </p>
+          <p style={{ fontSize: '16px', margin: '0', marginBottom: '10px' }}>
+            Available Room Count: <b>{hotel.rooms}</b>
+          </p>{' '}
+          <a href="/paymentgateway">
+            <Button
+              variant="secondary"
+              style={{
+                color: 'white',
+                borderRadius: '10px',
+                padding: '10px',
+                width: '200px',
+                backgroundColor: 'black',
+                border: 'none',
+                fontSize: '16px',
+                cursor: 'pointer',
+              }}
+            >
+              Book Now
+            </Button>{' '}
+          </a>
+        </div>{' '}
+      </div>{' '}
+      <Footer_3 />
+    </div>
+  );
+};
+
+export default ViewHotel;
